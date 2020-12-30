7e2766ccb9f457ac8eb775ab1382b77e5a9e5de0
Compartany <2994359861@qq.com>
2020-12-30 23:52:12 +0800

    optimize package.bat

--- package.bat
+++ package.bat
@@ -3,6 +3,7 @@
 @REM 切换编码为 UTF-8
 CHCP 65001
 
+SET zip=%ProgramFiles%/WinRAR/WinRAR
 SET archive=archive
 SET package=EnvManipulators.zip
 
@@ -16,11 +17,11 @@ if EXIST %package% (
 )
 
 @REM -r 递归
-"%ProgramFiles%/WinRAR/WinRAR" a -r %package% img\
-"%ProgramFiles%/WinRAR/WinRAR" a -r %package% scripts\
-"%ProgramFiles%/WinRAR/WinRAR" a  %package% "说明.txt"
-"%ProgramFiles%/WinRAR/WinRAR" a  %package% "README.txt"
-"%ProgramFiles%/WinRAR/WinRAR" a  %package% "version.txt"
+"%zip%" a -r %package% img\
+"%zip%" a -r %package% scripts\
+"%zip%" a  %package% "说明.txt"
+"%zip%" a  %package% "README.txt"
+"%zip%" a  %package% "version.txt"
 ECHO Package %package%
 
 COPY %package% %archive%\%version%.zip
