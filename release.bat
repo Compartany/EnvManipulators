a740b400e38e86f7ca55c2e8812c60b1c23ad217
Compartany <2994359861@qq.com>
2021-01-09 19:58:09 +0800

    release.bat 增加确认

--- release.bat
+++ release.bat
@@ -1,7 +1,16 @@
 @ECHO OFF
 
+@REM 切换编码为 UTF-8
+CHCP 65001
+
+:PROMT
+SET /P CONFIRM=确认发行吗？请确保本地更新已发布到远程仓库！(y/N)
+IF /I "%CONFIRM%" NEQ "y" GOTO END
+
 SET /P version=<version.txt
 ECHO Version: %version%
 
 gh release create v%version% EnvManipulators.zip -t v%version%
+
+:END
 ECHO done
