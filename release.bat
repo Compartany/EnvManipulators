706b6c825f05bc8d976eec66399ce5444b37a2eb
Compartany <2994359861@qq.com>
2021-01-09 18:54:55 +0800

    update release.bat

--- release.bat
+++ release.bat
@@ -1,4 +1,7 @@
+@ECHO OFF
+
 SET /P version=<version.txt
 ECHO Version: %version%
 
-gh release create v%version% EnvManipulators.zip
+gh release create v%version% EnvManipulators.zip -t v%version%
+ECHO done
